52a62b3800fea94f66fc041ed7571820937e705e
Adam Pluth <Apluth@crimson.ua.edu>
2017-02-07 10:47:16 -0600

    c files and makefile

---
+++ Assign1/collatz.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // fprintf, printf
+#include <unistd.h> // fork()
+#include <sys/types.h> // pid_t
+
+
+int main(int argc, char** argv) {
+	printf("collatz ran\n");
+	return 0;
+}
