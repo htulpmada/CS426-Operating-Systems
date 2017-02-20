f1b0ee94d6c9a6ee6659770e55f98a7d44c292e7
Adam Pluth <Apluth@crimson.ua.edu>
2017-02-20 06:48:39 -0600

    enough to start

---
+++ Assign2/pid_manager.c
@@ -0,0 +1,24 @@
+//////////////////
+//  Adam Pluth  //
+//  CS 426 OS   //
+//   2/14/17    //
+// pid_manager  //
+//////////////////
+
+
+#include <stdio.h> // fprintf, printf
+#include <unistd.h> // fork()
+#include <sys/types.h> // pid_t
+#include <sys/wait.h> // wait();
+#include <stdlib.h> // wait();
+
+pid_t id;
+
+
+int main(int argc, char** argv) {
+
+	// program start //
+
+	return 0;
+}
+
